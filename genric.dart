f3bee28694cafd77a5a34ef50cf00e9e248812db
Falmban Ammar <devAspirant@gmail.com>
2018-12-13 11:02:26 +0300

    Create genric.dart

---
+++ genric.dart
@@ -0,0 +1,13 @@
+void main() {
+ List<int> numbers = new List<int>();
+ numbers.addAll([1,2,3,4]);
+ print(numbers); 
+ List<String> letters = new List<String>();
+ letters.addAll(['a','m']);
+ print(letters);  
+	addNumbers<int>(1,2);
+}
+
+void addNumbers<T extends num>(T a, T b){
+  print(a + b);
+}
